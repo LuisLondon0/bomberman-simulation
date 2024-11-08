70a7635b297880089f86c055e7cfe3bc5c982e37
Luis Fernando Patiño Londoño <luizlondono@gmail.com>
2024-11-07 19:09:34 -0500

    minimal adjustments to hill climbing search

--- searches/informed_searchs/hill_climbing.py
+++ searches/informed_searchs/hill_climbing.py
@@ -33,53 +33,54 @@ class HillClimbingSearch(SearchStrategy):
         while stack:
             level += 1
             current_position, _, path = stack.pop()
-            agent.model.grid[current_position[0]][current_position[1]][0].visit_order += " (" + str(step) + ")"
-            self.visited.add(current_position)
-            step += 1
+            if current_position not in self.visited:
+                agent.model.grid[current_position[0]][current_position[1]][0].visit_order += " (" + str(step) + ")"
+                self.visited.add(current_position)
+                step += 1
 
-            if current_position == self.goal:
-                print("-------------------------------------")
-                print("VECES QUE VUELVE ATRAS: ", self.back_nums)
-                print("-------------------------------------")
-                return path
+                if current_position == self.goal:
+                    print("-------------------------------------")
+                    print("VECES QUE VUELVE ATRAS: ", self.back_nums)
+                    print("-------------------------------------")
+                    return path
 
-            neighbors = []
-            for dx, dy in directions:
-                new_x, new_y = current_position[0] + dx, current_position[1] + dy
-                new_position = (new_x, new_y)
+                neighbors = []
+                for dx, dy in directions:
+                    new_x, new_y = current_position[0] + dx, current_position[1] + dy
+                    new_position = (new_x, new_y)
 
-                if (
-                    0 <= new_x < agent.model.grid.width
-                    and 0 <= new_y < agent.model.grid.height
-                    and new_position not in self.visited
-                ):
-                    agents_in_new_cell = agent.model.grid[new_x][new_y]
-                    if all(isinstance(a, (RoadAgent, GoalAgent, EnemyAgent, RockAgent)) for a in agents_in_new_cell):
-                        heuristic_cost = heuristic(new_position, self.goal, self.heuristic_type)
-                        direction_priority = priority_order.get((dx, dy), float('inf'))
-                        neighbors.append((heuristic_cost, direction_priority, new_position))
+                    if (
+                        0 <= new_x < agent.model.grid.width
+                        and 0 <= new_y < agent.model.grid.height
+                        and new_position not in self.visited
+                    ):
+                        agents_in_new_cell = agent.model.grid[new_x][new_y]
+                        if all(isinstance(a, (RoadAgent, GoalAgent, EnemyAgent, RockAgent)) for a in agents_in_new_cell):
+                            heuristic_cost = heuristic(new_position, self.goal, self.heuristic_type)
+                            direction_priority = priority_order.get((dx, dy), float('inf'))
+                            neighbors.append((heuristic_cost, direction_priority, new_position))
 
-            aux = 0
-            if neighbors:
-                neighbors.sort(key=lambda x: (x[0], x[1]), reverse=True)
-                for _, _, neighbor in neighbors:
-                    if not agent.model.grid[neighbor[0]][neighbor[1]][0].visit_order:
-                        stack.append((neighbor, level, path + [neighbor]))
-                        agent.model.grid[neighbor[0]][neighbor[1]][0].visit_order = str(level)
-                        aux += 1
+                aux = 0
+                if neighbors:
+                    neighbors.sort(key=lambda x: (x[0], x[1]), reverse=True)
+                    for _, _, neighbor in neighbors:
+                        if not agent.model.grid[neighbor[0]][neighbor[1]][0].visit_order:
+                            stack.append((neighbor, level, path + [neighbor]))
+                            agent.model.grid[neighbor[0]][neighbor[1]][0].visit_order = str(level)
+                            aux += 1
 
-            if aux == 0:
-                self.back_nums += 1
-                level -= 1
-                unvisited_positions = [
-                    (lvl, heuristic(pos, self.goal, self.heuristic_type), pos, pth)
-                    for pos, lvl, pth in stack
-                    if pos not in self.visited
-                ]
-                
-                if unvisited_positions:
-                    unvisited_positions.sort(key=lambda x: (x[0], x[1]))
-                    _, _, best_position, best_path_to_position = unvisited_positions[0]
-                    stack.append((best_position, level, best_path_to_position + [best_position]))
+                if aux == 0:
+                    self.back_nums += 1
+                    level -= 1
+                    unvisited_positions = [
+                        (lvl, heuristic(pos, self.goal, self.heuristic_type), pos, pth)
+                        for pos, lvl, pth in stack
+                        if pos not in self.visited
+                    ]
+                    
+                    if unvisited_positions:
+                        unvisited_positions.sort(key=lambda x: (x[0], x[1]))
+                        _, _, best_position, best_path_to_position = unvisited_positions[0]
+                        stack.append((best_position, level, best_path_to_position + [best_position]))
 
         return best_path
